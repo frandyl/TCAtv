3748d28646f89e06790039affa80a6ed9075dad3
frandyl <frandylaryea@gmail.com>
2023-06-17 00:05:34 +0000

    added the start

--- src/App.js
+++ src/App.js
@@ -1,133 +1,45 @@
 import "./App.css";
 import "./button.css";
 import "./diffbuton.css";
-import {
-  CarouselComponent,
-  CarouselItemsDirective,
-  CarouselItemDirective,
-} from "@syncfusion/ej2-react-navigations";
+import ImageSlider from "./ImageSlider.js";
 import tca from "./tca.png";
 
 function App() {
-  const slide1Template = () => (
-    <figure className="img-container">
-      <img
-        src="src\imagefolder\DEout.jpg"
-        alt="German"
-        style={{ height: "100%", width: "100%" }}
-      />
-      <figcaption className="img-caption">
-        This is Hdeadline , major news
-      </figcaption>
-    </figure>
-  );
+  const slides = [
+    { url: "http://localhost:3000/image-1.jpg", title: "beach" },
+    { url: "http://localhost:3000/image-2.jpg", title: "boat" },
+    { url: "http://localhost:3000/image-3.jpg", title: "forest" },
+    { url: "http://localhost:3000/image-4.jpg", title: "city" },
+    { url: "http://localhost:3000/image-5.jpg", title: "italy" },
+    { url: "http://localhost:3000/image-6.jpg", title: "italy" },
+    { url: "http://localhost:3000/image-7.jpg", title: "italy" },
+  ];
 
-  const slide2Template = () => (
-    <figure className="img-container">
-      <img
-        src="src\imagefolder\fli-launch.jpg"
-        alt="white"
-        style={{ height: "100%", width: "100%" }}
-      />
-      <figcaption className="img-caption">
-        This is Hdeadline , major news
-      </figcaption>
-    </figure>
-  );
+  const containerStyles = {
+    width: "fit",
+    height: "400px",
+    margin: "0 auto",
+    position: "center",
+  };
 
-  const slide3Template = () => (
-    <figure className="img-container">
-      <img
-        src="src\imagefolder\iwind.jpg"
-        alt="blue"
-        style={{ height: "100%", width: "100%" }}
-      />
-      <figcaption className="img-caption">
-        This is Hdeadline , major news
-      </figcaption>
-    </figure>
-  );
-
-  const slide4Template = () => (
-    <figure className="img-container">
-      <img
-        src="src\imagefolder\NIKE-CAMPAIGNGB.jpg"
-        alt="green goo"
-        style={{ height: "100%", width: "100%" }}
-      />
-      <figcaption className="img-caption">
-        This is Hdeadline , major news
-      </figcaption>
-    </figure>
-  );
-
-  const slide5Template = () => (
-    <figure className="img-container">
-      <img
-        src="src\imagefolder\pivotal-statement.jpg"
-        alt="blackwhite"
-        style={{ height: "100%", width: "100%" }}
-      />
-      <figcaption className="img-caption">
-        This is Hdeadline , major news
-      </figcaption>
-    </figure>
-  );
-
-  const slide6Template = () => (
-    <figure className="img-container">
-      <img
-        src="src\imagefolder\tiedye.jpg"
-        alt="tiedye"
-        style={{ height: "100%", width: "100%" }}
-      />
-      <figcaption className="img-caption">
-        This is Hdeadline , major news
-      </figcaption>
-    </figure>
-  );
-
-  const slide7Template = () => (
-    <figure className="img-container">
-      <img
-        src="src\imagefolder\wpfd.jpg"
-        alt="press"
-        style={{ height: "100%", width: "100%" }}
-      />
-      <figcaption className="img-caption">
-        This is Hdeadline , major news
-      </figcaption>
-    </figure>
-  );
   return (
-    <div className="App" style={{ display: "flex" }}>
-      <button className="diffbuton">
-        <img src={tca} imagewidth={45} height={45} />
-      </button>
-      <button className="buttonstyle" style={{ marginLeft: "auto" }}>
-        News
-      </button>
-      <button className="buttonstyle">Sports</button>
-      <button className="buttonstyle">Entertainment</button>
-      <button className="buttonstyle">Travel</button>
-      <button className="buttonstyle">Culture</button>
-
-      <div className="carousel-sample">
-        <CarouselComponent cssClass="default-carousel">
-          <CarouselItemsDirective>
-            <CarouselItemDirective>
-              template={slide1Template}
-              template={slide2Template}
-              template={slide3Template}
-              template={slide4Template}
-              template={slide5Template}
-              template={slide6Template}
-              template={slide7Template}
-            </CarouselItemDirective>
-          </CarouselItemsDirective>
-        </CarouselComponent>
+    <>
+      <div className="App" style={{ display: "flex" }}>
+        <button className="diffbuton">
+          <img src={tca} imagewidth={45} height={45} />
+        </button>
+        <button className="buttonstyle" style={{ marginLeft: "auto" }}>
+          News
+        </button>
+        <button className="buttonstyle">Sports</button>
+        <button className="buttonstyle">Entertainment</button>
+        <button className="buttonstyle">Travel</button>
+        <button className="buttonstyle">Culture</button>
       </div>
-    </div>
+      <div style={containerStyles}>
+        <ImageSlider slides={slides} />
+      </div>
+    </>
   );
 }
 
---
+++ src/ImageSlider.js
@@ -0,0 +1,98 @@
+import { useState } from "react";
+import "./bullets.css";
+import leftarrow from "./leftarrow.png";
+import rightarrow from "./rightarrow.png";
+
+const slideStyles = {
+  width: "100%",
+  height: "100%",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+};
+
+const leftArrowStyles = {
+  position: "absolute",
+  top: "50%",
+  transform: "translate(0, -50%)",
+  left: "32px",
+  fontSize: "45px",
+  color: "#fff",
+  zIndex: 1,
+  cursor: "pointer",
+};
+
+const rightArrowStyles = {
+  position: "absolute",
+  top: "50%",
+  transform: "translate(0, -50%)",
+  right: "32px",
+  fontSize: "45px",
+  color: "#fff",
+  zIndex: 1,
+  cursor: "pointer",
+};
+
+const sliderStyles = {
+  position: "relative",
+  height: "100%",
+};
+
+const dotsContainerStyles = {
+  display: "flex",
+  justifyContent: "center",
+  top: "538px",
+};
+
+const dotStyle = {
+  margin: "0 3px",
+  cursor: "pointer",
+  fontSize: "20px",
+};
+
+const ImageSlider = ({ slides }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const goToPrevious = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+  const goToNext = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
+  const slideStylesWidthBackground = {
+    ...slideStyles,
+    backgroundImage: `url(${slides[currentIndex].url})`,
+  };
+
+  return (
+    <div style={sliderStyles}>
+      <div>
+        <div onClick={goToPrevious} style={leftArrowStyles}>
+          <img src={leftarrow} />
+        </div>
+        <div onClick={goToNext} style={rightArrowStyles}>
+          <img src={rightarrow} />
+        </div>
+      </div>
+      <div style={slideStylesWidthBackground}></div>
+      <div style={dotsContainerStyles}>
+        {slides.map((slide, slideIndex) => (
+          <div
+            style={dotStyle}
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+          >
+            <div className="bullet"></div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ImageSlider;
