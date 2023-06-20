2866e8432f3f5d01cce85462e4c8f0d8afe2e36e
frandyl <frandylaryea@gmail.com>
2023-06-20 17:44:28 +0000

    Imitial commit

--- src/App.js
+++ src/App.js
@@ -6,18 +6,44 @@ import tca from "./tca.png";
 
 function App() {
   const slides = [
-    { url: "http://localhost:3000/image-1.jpg", title: "beach" },
-    { url: "http://localhost:3000/image-2.jpg", title: "boat" },
-    { url: "http://localhost:3000/image-3.jpg", title: "forest" },
-    { url: "http://localhost:3000/image-4.jpg", title: "city" },
-    { url: "http://localhost:3000/image-5.jpg", title: "italy" },
-    { url: "http://localhost:3000/image-6.jpg", title: "italy" },
-    { url: "http://localhost:3000/image-7.jpg", title: "italy" },
+    {
+      url: "http://localhost:3000/image-1.jpg",
+      title: "beach",
+      textColor: "red",
+    },
+    {
+      url: "http://localhost:3000/image-2.jpg",
+      title: "This Is the news Headlines of the day",
+      textColor: "red",
+    },
+    {
+      url: "http://localhost:3000/image-3.jpg",
+      title: "This Is the news Headlines of the day",
+      textColor: "red",
+    },
+    {
+      url: "http://localhost:3000/image-4.jpg",
+      title: "This Is the news Headlines of the day",
+      textColor: "red",
+    },
+    {
+      url: "http://localhost:3000/image-5.jpg",
+      title: "This Is the news Headlines of the day",
+      textColor: "red",
+    },
+    {
+      url: "http://localhost:3000/image-6.jpg",
+      title: "This Is the news Headlines of the day",
+    },
+    {
+      url: "http://localhost:3000/image-7.jpg",
+      title: "This Is the news Headlines of the day",
+    },
   ];
 
   const containerStyles = {
     width: "fit",
-    height: "400px",
+    height: "500px",
     margin: "0 auto",
     position: "center",
   };
@@ -26,7 +52,7 @@ function App() {
     <>
       <div className="App" style={{ display: "flex" }}>
         <button className="diffbuton">
-          <img src={tca} imagewidth={45} height={45} />
+          <img src={tca} width={45} height={45} />
         </button>
         <button className="buttonstyle" style={{ marginLeft: "auto" }}>
           News
@@ -37,7 +63,7 @@ function App() {
         <button className="buttonstyle">Culture</button>
       </div>
       <div style={containerStyles}>
-        <ImageSlider slides={slides} />
+        <ImageSlider slides={slides} parentWidth={2000} />
       </div>
     </>
   );
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import "./bullets.css";
 import leftarrow from "./leftarrow.png";
 import rightarrow from "./rightarrow.png";
@@ -8,6 +8,7 @@ const slideStyles = {
   height: "100%",
   backgroundSize: "cover",
   backgroundPosition: "center",
+  transition: "opacity 0.55s ease-in",
 };
 
 const leftArrowStyles = {
@@ -40,22 +41,40 @@ const sliderStyles = {
 const dotsContainerStyles = {
   display: "flex",
   justifyContent: "center",
-  top: "538px",
+  top: "50%",
+  transform: "translate(0, -140%)",
+  zIndex: 3,
 };
 
 const dotStyle = {
   margin: "0 3px",
   cursor: "pointer",
   fontSize: "20px",
+  zIndex: 3,
+  backgroundColor: "#000000",
+};
+
+const activeDotStyle = {
+  ...dotStyle,
+  backgroundColor: "#00ff2a",
+  transform: "translate(0, -300%)",
+};
+
+const inactiveDotStyle = {
+  ...dotStyle,
+  backgroundColor: "#808080", // Color for the inactive slide's bullets
+  transform: "translate(0, -300%)",
 };
 
 const ImageSlider = ({ slides }) => {
+  const timerRef = useRef(null);
   const [currentIndex, setCurrentIndex] = useState(0);
   const goToPrevious = () => {
     const isFirstSlide = currentIndex === 0;
     const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex);
   };
+
   const goToNext = () => {
     const isLastSlide = currentIndex === slides.length - 1;
     const newIndex = isLastSlide ? 0 : currentIndex + 1;
@@ -69,6 +88,17 @@ const ImageSlider = ({ slides }) => {
     backgroundImage: `url(${slides[currentIndex].url})`,
   };
 
+  useEffect(() => {
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
+    timerRef.current = setTimeout(() => {
+      goToNext();
+    }, 5000);
+
+    return () => clearTimeout(timerRef.current);
+  }, [goToNext]);
+
   return (
     <div style={sliderStyles}>
       <div>
@@ -79,16 +109,44 @@ const ImageSlider = ({ slides }) => {
           <img src={rightarrow} />
         </div>
       </div>
-      <div style={slideStylesWidthBackground}></div>
+
+      <div style={slideStylesWidthBackground}>
+        <div
+          style={{
+            position: "absolute",
+            bottom: 0,
+            left: 0,
+            width: "100%",
+            height: "100px",
+            background:
+              "linear-gradient(to bottom, rgba(0,0,0,0), rgba(0,0,0,1))",
+            zIndex: 0,
+          }}
+        ></div>
+        <h1
+          style={{
+            position: "absolute",
+            bottom: "20px",
+            left: "20px",
+            color: slides[currentIndex].textColor,
+            zIndex: 2,
+            opacity: 1,
+            transition: "opacity 0.5s ease in",
+          }}
+        >
+          {slides[currentIndex].title}
+        </h1>
+      </div>
       <div style={dotsContainerStyles}>
         {slides.map((slide, slideIndex) => (
           <div
-            style={dotStyle}
-            key={slideIndex}
+            style={
+              slideIndex === currentIndex ? activeDotStyle : inactiveDotStyle
+            }
+            className={slideIndex === currentIndex ? "bullet-active" : "bullet"}
+            key={slideIndex === currentIndex ? activeDotStyle : dotStyle}
             onClick={() => goToSlide(slideIndex)}
-          >
-            <div className="bullet"></div>
-          </div>
+          ></div>
         ))}
       </div>
     </div>
